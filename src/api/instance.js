0426e5c0a198a4a480a8dac9c2d547107648200f
Elyric Manatad <elyric.manatad@sun-asterisk.com>
2023-02-17 11:25:53 +0800

    https update on instance

--- src/api/instance.js
+++ src/api/instance.js
@@ -4,13 +4,13 @@ const token = localStorage.getItem("token");
 const admin_token = localStorage.getItem("admin_token");
 
 export const userInstance = axios.create({
-  baseURL: "http://phplaravel-943133-3282971.cloudwaysapps.com/api",
+  baseURL: "https://phplaravel-943133-3282971.cloudwaysapps.com/api",
   headers: {
     Authorization: `Bearer ${token}`,
   },
 });
 export const adminInstance = axios.create({
-  baseURL: "http://phplaravel-943133-3282971.cloudwaysapps.com//api/admin",
+  baseURL: "https://phplaravel-943133-3282971.cloudwaysapps.com/api/admin",
   headers: {
     Authorization: `Bearer ${admin_token}`,
   },
